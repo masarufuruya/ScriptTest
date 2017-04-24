50594103c88c5ab3c14fbb6f175067c69387e286
古家大 <yannwennri2@gmail.com>
2017-04-24 16:44:49 +0900

    complete kadai

---
+++ Assets/Boss.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+	//体力
+	private int hp;
+	//攻撃力
+	private int power = 10;
+	//魔力
+	private int mp = 53;
+
+	//攻撃
+	public void Attack() {
+		Debug.Log (this.power + "のダメージを与えた");
+	}
+
+	//魔法攻撃
+	public void Magic() {
+		if ((this.mp - 5) < 0) {
+			Debug.Log ("MPが足りないため魔法が使えない。");
+			return;
+		}
+		this.mp -= 5;
+		Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
+	}
+
+	//防御
+	public void Defence(int damage) {
+		Debug.Log (damage + "のダメージを受けた");
+		this.hp -= damage;
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//5個のint型の配列arrayを宣言して初期化
+		int[] heights = {170,175,160,180,150};
+		//配列の各要素の値を順番に表示
+		for (int i = 0; i < heights.Length; i++) {
+			Debug.Log (heights [i]);
+		}
+		//配列の各要素を逆に表示
+		int length = heights.Length - 1;
+		for (int i = length; i >= 0; i--) {
+			Debug.Log (heights [i]);
+		}
+		//↓発展課題
+		Boss boss = new Boss();
+		for (int i = 11; i > 0; i--) {
+			boss.Magic ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
